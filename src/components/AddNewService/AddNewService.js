ab4ff64dd4bd7d02290dea10fe82cfaa293d48db
mdshofiur <shofiurbd13@gmail.com>
2021-10-29 23:43:10 +0600

    added

--- src/components/AddNewService/AddNewService.js
+++ src/components/AddNewService/AddNewService.js
@@ -1,13 +1,23 @@
 import React from 'react';
 import './AddNewService.css'
 import { useForm } from "react-hook-form";
-
+import axios from 'axios';
 
 const AddNewService = () => {
 
-const { register, handleSubmit, formState: { errors } } = useForm();
-  const onSubmit = data => console.log(data);
+const { register, handleSubmit,reset } = useForm();
 
+  const onSubmit = data => {
+    axios.post('http://localhost:5000/addnewservice',data)
+    .then(res => {
+       if(res.data.insertedId) {
+           alert("Sucessfully added")
+           reset();
+       }
+      })
+    };
+
+    
 
     return (
      <div>
@@ -16,9 +26,8 @@ const { register, handleSubmit, formState: { errors } } = useForm();
     <form className="container" onSubmit={handleSubmit(onSubmit)}>
     <input placeholder="img" {...register("img")} />
     <input placeholder="title" {...register("title")} />
-    <input placeholder="Place name" {...register("Place", { required: true })} />
-    {errors.exampleRequired && <span>This field is required</span>}
-    <input placeholder="desc" {...register("desc")} />
+    <input placeholder="Place name" {...register("Place", { required: true })}/>
+    <textarea placeholder="desc" {...register("desc")} />
     <input placeholder="price" {...register("price")} />
     <input className="service-button" type="submit" />
   </form>
--- src/components/BookingItem/BookingItem.js
+++ src/components/BookingItem/BookingItem.js
@@ -1,72 +1,45 @@
-import React from 'react';
+
 import './BookingItem.css'
+import { useState ,useEffect} from 'react';
+
 
 const BookingItem = () => {
+    const [service, setservice] =  useState([]);
+
+     useEffect(() => {
+        fetch('http://localhost:5000/addnewservice')
+            .then(res => res.json())
+            .then(data => setservice(data));
+    }, [])
+
+
     return (
-        <div className="container">
-          <div className="booking">
 
-
-            <div className="booking-item">
-               <div>
-               <img src="https://i.ibb.co/x1n2Z0h/5c1e472975fcbd26856e1b0b-Kilimanjaro-sunrise-copy-820x520.jpg" alt="" />
-                <h4>Mount Kilimanjaro, Machame Route</h4>
-                <h5 className="location">Bryce Canyon National Park, USA</h5>
-                <p>Approaching legendary Kilimanjaro from the south-west, we take the popular Machame Route,with a total trip time of nine days</p>
+        <div className="container booking">
+          {
+              service.map(service => <div key={service._id}>
+                 
+                 <div className="booking-item">
+                <div>
+               <img src={service.img} alt="" />
+                <h4>{service.title.slice(0,27)}</h4>
+                <h5 className="location">{service.Place}</h5>
+                <p>{service.desc.slice(0,100)}</p>
                </div>
-
                    <div className="book-section">
                     <div>
-                    <h5 className="price text-danger">From $100</h5>
+                    <h5 className="price text-danger">From ${service.price}</h5>
                     </div>
-                    
                     <div className="btn-section">
                     <button className="btn btn-primary">Book Now</button>
                     </div>
                    </div>
                     </div>
+              </div> )
+          }
+         </div>
 
-                    <div className="booking-item">
-               <div>
-               <img src="https://i.ibb.co/x1n2Z0h/5c1e472975fcbd26856e1b0b-Kilimanjaro-sunrise-copy-820x520.jpg" alt="" />
-                <h4>Mount Kilimanjaro, Machame Route</h4>
-                <h5 className="location">Bryce Canyon National Park, USA</h5>
-                <p>Approaching legendary Kilimanjaro from the south-west, we take the popular Machame Route,with a total trip time of nine days</p>
-               </div>
-
-                   <div className="book-section">
-                    <div>
-                    <h5 className="price text-danger">From $100</h5>
-                    </div>
-                    
-                    <div className="btn-section">
-                    <button className="btn btn-primary">Book Now</button>
-                    </div>
-                   </div>
-                    </div>
-
-                    
-                    <div className="booking-item">
-               <div>
-               <img src="https://i.ibb.co/x1n2Z0h/5c1e472975fcbd26856e1b0b-Kilimanjaro-sunrise-copy-820x520.jpg" alt="" />
-                <h4>Mount Kilimanjaro, Machame Route</h4>
-                <h5 className="location">Bryce Canyon National Park, USA</h5>
-                <p>Approaching legendary Kilimanjaro from the south-west, we take the popular Machame Route,with a total trip time of nine days</p>
-               </div>
-
-                   <div className="book-section">
-                    <div>
-                    <h5 className="price text-danger">From $100</h5>
-                    </div>
-                    
-                    <div className="btn-section">
-                    <button className="btn btn-primary">Book Now</button>
-                    </div>
-                   </div>
-                    </div>
-
-          </div>
-        </div>
+                 
     );
 };
 
